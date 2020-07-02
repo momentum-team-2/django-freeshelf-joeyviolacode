d0d8b0179d8c3538016f538e34f14bf1e8274b7e
joeyviolacode <joeyviolacode@gmail.com>
2020-07-01 22:53:39 -0400

    Logins required on all pages, no welcome page made.  Some tiny layout tweaks to header bar.

--- core/views.py
+++ core/views.py
@@ -11,32 +11,38 @@ def list_books(request):
     categories = Category.objects.all()
     return render(request, 'core/list_books.html', { "books" : books, "categories" : categories })
 
+@login_required
 def list_books_oldest(request):
     books = Book.objects.all
     categories = Category.objects.all()
     return render(request, 'core/list_books.html', { "books" : books, "categories" : categories })
 
+@login_required
 def list_books_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     categories = Category.objects.all()
     return render(request, 'core/list_category.html', {"category":category, "categories":categories})
 
+@login_required
 def list_books_title(request):
     books = Book.objects.order_by("title")
     categories = Category.objects.all()
     return render(request, 'core/list_books.html', { "books" : books, "categories" : categories })
 
+@login_required
 def show_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     categories = Category.objects.all()
     form = CommentForm()
     return render(request, 'core/show_book.html', { "book" : book, "categories" : categories, "form":form })
 
+@login_required
 def show_user(request, pk): 
     user = get_object_or_404(User, pk=pk)
     comments = user.comments.all
     return render(request, 'core/show_user.html', { "user": user, "comments" : comments})
 
+@login_required
 def add_comment(request, pk):
     book = get_object_or_404(Book, pk=pk)
     user = request.user
@@ -49,18 +55,21 @@ def add_comment(request, pk):
         comment.save()
     return redirect(to="show_book", pk=pk)
 
+@login_required
 def mark_favorite(request, pk):
     book = get_object_or_404(Book, pk=pk)
     user = request.user
     book.favorited_by.add(user)
     return redirect(to="show_book", pk=pk)
 
+@login_required
 def mark_not_favorite(request, pk):
     book = get_object_or_404(Book, pk=pk)
     user = request.user
     book.favorited_by.remove(user)
     return redirect(to="show_book", pk=pk)
 
+@login_required
 def show_favorites(request):
     user = request.user
     books = user.favorite_books.all()
